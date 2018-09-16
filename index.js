e9d346fd1a9ca1f8e64ec7cf095f8218d39cef1e
Kapil Munish <kapilmunish@gmail.com>
2018-09-16 23:27:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function logWhisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase())
 }
+
+function sayHiToGrandma(string) {
+  if(string ==='hello')
+    return "I can\'t hear you!";
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
